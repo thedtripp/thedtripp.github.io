427260d73fd30a9271fe45a14e1363e025b0e9be
thedtripp <thedtripp@gmail.com>
2023-01-26 12:22:20 -0700

    refactored to remove references to pkmn cards

--- scripts/projectcardscript.js
+++ scripts/projectcardscript.js
@@ -1,17 +1,5 @@
-/*
-  PKMN
-  using 
-    - an animated gif of sparkles.
-    - an animated gradient as a holo effect.
-    - color-dodge mix blend mode
-  
-*/
-
-console.log("hello")
-
-
 var x;
-var $cards = $(".pknmcard");
+var $cards = $(".projectcard");
 var $style = $(".hover");
 
 $cards
@@ -22,12 +10,12 @@ $cards
     if ( e.type === "touchmove" ) {
       pos = [ e.touches[0].clientX, e.touches[0].clientY ];
     }
-    var $pknmcard = $(this);
+    var $projectcard = $(this);
     // math for mouse position
     var l = pos[0];
     var t = pos[1];
-    var h = $pknmcard.height();
-    var w = $pknmcard.width();
+    var h = $projectcard.height();
+    var w = $projectcard.width();
     var px = Math.abs(Math.floor(100 / w * l)-100);
     var py = Math.abs(Math.floor(100 / h * t)-100);
     var pa = (50-px)+(50-py);
@@ -39,20 +27,20 @@ $cards
     var p_opc = 20+(Math.abs(pa)*1.5);
     var ty = ((tp - 50)/2) * -1;
     var tx = ((lp - 50)/1.5) * .5;
-    // css to apply for active pknmcard
+    // css to apply for active projectcard
     var grad_pos = `background-position: ${lp}% ${tp}%;`
     var sprk_pos = `background-position: ${px_spark}% ${py_spark}%;`
     var opc = `opacity: ${p_opc/100};`
     var tf = `transform: rotateX(${ty}deg) rotateY(${tx}deg)`
     // need to use a <style> tag for psuedo elements
     var style = `
-      .pknmcard:hover:before { ${grad_pos} }  /* gradient */
-      .pknmcard:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
+      .projectcard:hover:before { ${grad_pos} }  /* gradient */
+      .projectcard:hover:after { ${sprk_pos} ${opc} }   /* sparkles */ 
     `
     // set / apply css class and style
     $cards.removeClass("active");
-    $pknmcard.removeClass("animated");
-    $pknmcard.attr( "style", tf );
+    $projectcard.removeClass("animated");
+    $projectcard.attr( "style", tf );
     $style.html(style);
     if ( e.type === "touchmove" ) {
       return false; 
@@ -60,10 +48,10 @@ $cards
     clearTimeout(x);
   }).on("mouseout touchend touchcancel", function() {
     // remove css, apply custom animation on end
-    var $pknmcard = $(this);
+    var $projectcard = $(this);
     $style.html("");
-    $pknmcard.removeAttr("style");
+    $projectcard.removeAttr("style");
     x = setTimeout(function() {
-      $pknmcard.addClass("animated");
+      $projectcard.addClass("animated");
     },2500);
   });
